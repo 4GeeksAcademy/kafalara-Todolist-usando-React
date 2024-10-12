2746fd105c39e5ddb45ac8d0eb33132872f8df85
Kafalara <karen.larrosa@gmail.com>
2024-10-12 00:22:15 +0000

    Proyecto entregado Aplicación de Todolist usando React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React from 'react';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setTodos] = React.useState([]);  
+  const [newTodo, setNewTodo] = React.useState("");  // nueva tarea
+
+  // Función que maneja la tecla Enter para agregar tareas
+  const handleKeyDown = (e) => {
+    if (e.code === "Enter" && newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);  // sumar tarea a la lista
+      setNewTodo("");  // quitar despues de agregar
+    }
+  };
+
+  return (
+    <>
+      <div className="text-center mt-5">
+      <h1 className ="todos-title">todos</h1>
+      </div>
+
+      {/* Uso de TodoInput como un componente y lo separo con un div diferente para el recuadro.*/}
+      <div className="home-container">
+      <TodoInput
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)} 
+        onKeyDown={handleKeyDown}  //tecla
+      />
+
+      {/* Renderiza la lista de tareas */}
+      <TodoList tasks={todos} removeTask={(id) => {
+        setTodos(todos.filter((task, index) => index !== id));
+      }} />
+
+      <p className="items-left">{todos.length} item left</p>
+    </div>
+    </>
+  );
 };
 
 export default Home;
